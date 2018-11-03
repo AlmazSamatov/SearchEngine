0bfa65167617c69deb9cbb1d747a614fce9d1299
Dima T <mrlocky98@gmail.com>
2018-11-03 22:22:20 +0300

    Multiple input files support was added

--- src/main/java/DocumentCounter.java
+++ src/main/java/DocumentCounter.java
@@ -1,3 +1,11 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.StringReader;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -6,41 +14,38 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.HashSet;
 
 public class DocumentCounter {
-    public static HashMap<String, Integer> docIds = new HashMap<String, Integer>();
-    public static int docCnt = 0;
 
     public static class TokenizerMapper
             extends Mapper<Object, Text, Text, IntWritable> {
 
-        private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
-        private IntWritable fileId = new IntWritable();
 
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
-            Integer docId = docIds.get(fileName);
-            if (docId == null) {
-                docIds.put(fileName, docCnt);
-                fileId.set(docCnt++);
-            } else {
-                fileId.set(docId);
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            try {
+                JSONObject json = new JSONObject(value.toString());
+                String text = json.getString("text");
+                int doc = json.getInt("id");
+
+                StringTokenizer itr = new StringTokenizer(text);
+                while (itr.hasMoreTokens()) {
+                    String token = itr.nextToken().toLowerCase();
+                    token = token.replaceAll("[^\\w&&[^-]]", "");
+                    word.set(token);
+                    IntWritable docId = new IntWritable(doc);
+                    context.write(word, docId);
+                }
             }
-
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                String token = itr.nextToken().toLowerCase();
-                token = token.replaceAll("[^\\w&&[^-]]", "");
-
-                word.set(token);
-                context.write(word, fileId);
+            catch (JSONException e){
+                e.printStackTrace();
             }
         }
     }
@@ -52,16 +57,14 @@ public class DocumentCounter {
         public void reduce(Text key, Iterable<IntWritable> values,
                            Context context
         ) throws IOException, InterruptedException {
-            // TODO: understand why everything works without set
-            //HashSet<Integer> docsUsed = new HashSet<Integer>();
+            HashSet<Integer> docsUsed = new HashSet<Integer>();
             int cnt = 0;
-            for (IntWritable val : values) {
-                cnt++;
-                /*int v = val.get();
+            for (IntWritable val: values) {
+                int v = val.get();
                 if (!docsUsed.contains(v)) {
                     cnt++;
                     docsUsed.add(v);
-                }*/
+                }
             }
             result.set(cnt);
             context.write(key, result);
@@ -73,12 +76,13 @@ public class DocumentCounter {
         Job job = Job.getInstance(conf, "document count");
         job.setJarByClass(DocumentCounter.class);
         job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(WordDocumentCount.class);
         job.setReducerClass(WordDocumentCount.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);//IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setOutputValueClass(IntWritable.class);
+        for (int i = 0; i < args.length - 1; i++) {
+            MultipleInputs.addInputPath(job, new Path(args[i]), TextInputFormat.class);
+        }
+        FileOutputFormat.setOutputPath(job, new Path(args[args.length - 1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
--- src/main/java/WordEnumerator.java
+++ src/main/java/WordEnumerator.java
@@ -1,3 +1,13 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.StringReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -7,50 +17,71 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
-import java.io.IOException;
-import java.util.StringTokenizer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 
 public class WordEnumerator {
-    private static int wordCounter = 0;
+  private static int wordCounter = 0;
 
-    public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, IntWritable> {
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
 
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
 
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                String token = itr.nextToken().toLowerCase();
-                token = token.replaceAll("[^\\w&&[^-]]", "");
-                word.set(token);
-                context.write(word, one);
-            }
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      BufferedReader reader = new BufferedReader(new StringReader(value.toString()));  
+      
+      JSONParser parser = new JSONParser();
+      String str;
+
+      try {
+        while ((str = reader.readLine()) != null) {
+          JSONObject json = (JSONObject) parser.parse(str);
+          String text = (String) json.get("text");
+          System.out.println((String) json.get("id"));
+          StringTokenizer itr = new StringTokenizer(text);
+          while (itr.hasMoreTokens()) {
+            String token = itr.nextToken().toLowerCase();
+            token = token.replaceAll("[^\\w&&[^-]]", "");
+            word.set(token);
+            context.write(word, one);
+          }
         }
+      }
+      catch (Exception e){
+          e.printStackTrace();
+      }
     }
+  }
 
-    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-        private IntWritable result = new IntWritable();
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
 
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            result.set(wordCounter++);
-            context.write(key, result);
-        }
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      result.set(wordCounter++);
+      context.write(key, result);
     }
+  }
 
-    public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(WordEnumerator.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(WordEnumerator.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    //FileInputFormat.addInputPath(job, new Path(args[0]));
+    for (int i = 0; i < args.length - 1; i++) {
+        MultipleInputs.addInputPath(job, new Path(args[i]), TextInputFormat.class);
     }
+    FileOutputFormat.setOutputPath(job, new Path(args[args.length - 1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
 }
