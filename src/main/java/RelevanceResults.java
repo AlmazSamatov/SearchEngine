30d0532c20c74da7de004b9756da20061a6c8ff8
AlmazSamatov <samatov.almaz@yandex.ru>
2018-11-04 22:23:47 +0300

    -

--- src/main/java/RelevanceResults.java
+++ src/main/java/RelevanceResults.java
@@ -1,6 +1,4 @@
 import com.google.gson.Gson;
-import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.WritableComparable;
 
 import java.io.DataInput;
@@ -8,48 +6,32 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class RelevanceResults implements WritableComparable<RelevanceResults> {
-    private IntWritable docId = new IntWritable();
-    private DoubleWritable relevance = new DoubleWritable();
-
-    public IntWritable getDocId() {
-        return docId;
-    }
-
-    public void setDocId(IntWritable docId) {
-        this.docId = docId;
-    }
-
-    public DoubleWritable getRelevance() {
-        return relevance;
-    }
-
-    public void setRelevance(DoubleWritable relevance) {
-        this.relevance = relevance;
-    }
+    private int docId;
+    private double relevance;
 
     RelevanceResults() {
     }
 
     RelevanceResults(int key, double value) {
-        docId.set(key);
-        relevance.set(value);
+        docId = key;
+        relevance = value;
     }
 
     @Override
     public int compareTo(RelevanceResults o) {
-        return Double.compare(o.relevance.get(), relevance.get());
+        return Double.compare(o.relevance, relevance);
     }
 
     @Override
     public void write(DataOutput dataOutput) throws IOException {
-        docId.write(dataOutput);
-        relevance.write(dataOutput);
+        dataOutput.writeInt(docId);
+        dataOutput.writeDouble(relevance);
     }
 
     @Override
     public void readFields(DataInput dataInput) throws IOException {
-        docId.readFields(dataInput);
-        relevance.readFields(dataInput);
+        docId = dataInput.readInt();
+        relevance = dataInput.readDouble();
     }
 
     @Override
