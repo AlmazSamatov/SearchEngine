ffbcca96162a6835ffc72599a9a8169d1b48c3f1
AlmazSamatov <samatov.almaz@yandex.ru>
2018-11-04 23:29:34 +0300

    -

--- src/main/java/ContentExtractor.java
+++ src/main/java/ContentExtractor.java
@@ -113,7 +113,8 @@ public class ContentExtractor {
         job.setJarByClass(ContentExtractor.class);
         job.setMapperClass(ContentExtractorMapper.class);
         job.setSortComparatorClass(ContentExtractorResultsComparator.class);
-        job.setReducerClass(ContentExtractorReducer.class);
+        //job.setReducerClass(ContentExtractorReducer.class);
+        job.setNumReduceTasks(0);
         job.setOutputKeyClass(OutputDocument.class);
         job.setOutputValueClass(NullWritable.class);
         for (int i = 0; i < args.length - 2; i++) {
